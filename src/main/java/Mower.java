5cecf3467e40fc16c4407103f3a67f2c97859c83
Houssem <houssi4ever@gmail.com>
2021-03-20 22:33:57 +0100

    Create Mower and Orientation classes

---
+++ src/main/java/Mower.java
@@ -0,0 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class Mower {
+
+    private int xPosition;
+
+    private int yPosition;
+
+    private Orientation orientation;
+
+}
