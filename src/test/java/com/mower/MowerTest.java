c2ebe78ba327a93ad2fc81c91e90145a1816a0ca
Houssem <houssi4ever@gmail.com>
2021-03-21 23:03:07 +0100

    refactor MowerTest

--- src/test/java/com/mower/MowerTest.java
+++ src/test/java/com/mower/MowerTest.java
@@ -8,47 +8,83 @@ import java.util.List;
 
 import static com.mower.Instruction.*;
 import static com.mower.Orientation.*;
+import static java.util.Collections.emptyList;
 
 
 public class MowerTest {
 
+    private Area area = Area.builder()
+            .height(5)
+            .width(5)
+            .build();
+
     @Test
     public void turnRight_Test() {
+        //GIVEN
         Mower mower = createMower(N);
+        //WHEN
         mower.turnRight();
-        Assert.assertEquals(E,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(E, mower.getOrientation());
+
+        //WHEN
         mower.turnRight();
-        Assert.assertEquals(S,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(S, mower.getOrientation());
+
+        //WHEN
         mower.turnRight();
-        Assert.assertEquals(W,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(W, mower.getOrientation());
+
+        //WHEN
         mower.turnRight();
-        Assert.assertEquals(N,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(N, mower.getOrientation());
 
     }
 
     @Test
     public void turnLeft_Test() {
+        //GIVEN
         Mower mower = createMower(N);
+        //WHEN
         mower.turnLeft();
-        Assert.assertEquals(W,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(W, mower.getOrientation());
+
+        //WHEN
         mower.turnLeft();
-        Assert.assertEquals(S,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(S, mower.getOrientation());
+
+        //WHEN
         mower.turnLeft();
-        Assert.assertEquals(E,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(E, mower.getOrientation());
+
+        //WHEN
         mower.turnLeft();
-        Assert.assertEquals(N,mower.getOrientation());
+        //THEN
+        Assert.assertEquals(N, mower.getOrientation());
 
     }
 
     @Test
     public void forward_north_initialized_Test() {
+        //GIVEN
         Mower mower = createMower(N);
+        //WHEN
         mower.forward();
-        Assert.assertEquals(1,mower.getYPosition());
+        //THEN
+        Assert.assertEquals(1, mower.getYPosition());
 
-        mower = createMower(5,5,N);
+        //GIVEN
+        mower = createMower(5, 5, N);
+        //WHEN
         mower.forward();
-        Assert.assertEquals(5,mower.getYPosition());
+        //THEN
+        Assert.assertEquals(5, mower.getYPosition());
 
     }
 
@@ -56,95 +92,76 @@ public class MowerTest {
     @Test
     public void forward_south_initialized_Test() {
         //GIVEN
-        Mower mower = createMower(3,2,S);
+        Mower mower = createMower(3, 2, S);
         //WHEN
         mower.forward();
         //THEN
-        Assert.assertEquals(1,mower.getYPosition());
+        Assert.assertEquals(1, mower.getYPosition());
 
         //GIVEN
         mower = createMower(S);
         //WHEN
         mower.forward();
         //THEN
-        Assert.assertEquals(0,mower.getYPosition());
+        Assert.assertEquals(0, mower.getYPosition());
 
     }
 
     @Test
     public void forward_east_initialized_Test() {
         //GIVEN
-        Mower mower = createMower(3,2,E);
+        Mower mower = createMower(3, 2, E);
         //WHEN
         mower.forward();
         //THEN
-        Assert.assertEquals(4,mower.getXPosition());
+        Assert.assertEquals(4, mower.getXPosition());
 
         //GIVEN
         mower = createMower(E);
         //WHEN
         mower.forward();
         //THEN
-        Assert.assertEquals(1,mower.getXPosition());
+        Assert.assertEquals(1, mower.getXPosition());
 
     }
 
     @Test
     public void forward_west_initialized_Test() {
         //GIVEN
-        Mower mower = createMower(3,2,W);
+        Mower mower = createMower(3, 2, W);
         //WHEN
         mower.forward();
         //THEN
-        Assert.assertEquals(2,mower.getXPosition());
+        Assert.assertEquals(2, mower.getXPosition());
 
         //GIVEN
         mower = createMower(W);
         //WHEN
         mower.forward();
         //THEN
-        Assert.assertEquals(0,mower.getXPosition());
+        Assert.assertEquals(0, mower.getXPosition());
 
     }
 
     @Test
     public void execute_Test() {
         //GIVEN
-        Mower mower = createMower(1,2, N, Arrays.asList(G,A,G,A,G,A,G,A,A));
+        Mower mower = createMower(1, 2, N, Arrays.asList(G, A, G, A, G, A, G, A, A));
         //WEN
         mower.execute();
         //THEN
-        Assert.assertEquals(1,mower.getXPosition());
-        Assert.assertEquals(3,mower.getYPosition());
-        Assert.assertEquals(N,mower.getOrientation());
-
+        Assert.assertEquals(1, mower.getXPosition());
+        Assert.assertEquals(3, mower.getYPosition());
+        Assert.assertEquals(N, mower.getOrientation());
     }
 
 
-    private Mower createMower(Orientation orientation){
-        Mower mower = Mower.builder()
-                .xPosition(0)
-                .yPosition(0)
-                .orientation(orientation)
-                .area(Area.builder()
-                        .height(5)
-                        .width(5)
-                        .build())
-                .build();
-        return mower;
+    private Mower createMower(Orientation orientation) {
+        return createMower(0, 0, orientation);
     }
 
-    private Mower createMower(int xPosition, int yPosition, Orientation orientation){
-        Mower mower = Mower.builder()
-                .xPosition(xPosition)
-                .yPosition(yPosition)
-                .orientation(orientation)
-                .area(Area.builder()
-                        .height(5)
-                        .width(5)
-                        .build())
-                .build();
-        return mower;
+    private Mower createMower(int xPosition, int yPosition, Orientation orientation) {
+        return createMower(xPosition,yPosition,orientation, emptyList());
     }
 
     private Mower createMower(int xPosition, int yPosition, Orientation orientation, List<Instruction> instructions) {
@@ -152,10 +169,7 @@ public class MowerTest {
                 .xPosition(xPosition)
                 .yPosition(yPosition)
                 .orientation(orientation)
-                .area(Area.builder()
-                        .height(5)
-                        .width(5)
-                        .build())
+                .area(area)
                 .instructions(instructions)
                 .build();
         return mower;
